e424a73d9bf6e405d31d5a76512394d70e0af462
Marcin Czapulak <73505362+Strzelacz48@users.noreply.github.com>
2022-11-16 17:28:25 +0100

    KFC_Runners Added

---
+++ KFC_Surfers/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    double upwardForce=100f;
+    bool DKey=false,Wkey=false,AKeay=false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetKeyDown)
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
